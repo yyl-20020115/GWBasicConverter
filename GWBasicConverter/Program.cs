70d1fbca3ea65f3c8af6cbec119cb529d90ae891
Yilin Yang <yyl_20050115@hotmail.com>
2024-06-10 13:00:06 +0800

    fx

--- GWBasicConverter/Program.cs
+++ GWBasicConverter/Program.cs
@@ -1,39 +1,36 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 
-namespace GWBasicConverter
+namespace GWBasicConverter;
+
+public class Program
 {
-    class Program
-    {
-       
-        static int Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("GWBasicConverter.exe input-file.bas output-file");
-            }
-            else
-            {
-                using var input = new FileStream(args[0], FileMode.Open);
-                using var output = new StreamWriter(args[1]);
-                var code = new GWBasicCodeGenerator();
-                try
-                {
-                    var lines = code.Parse(input,Encoding.UTF8); //Encoding.GetEncoding(437)
-                    if (lines != null)
-                    {
-                        lines.ForEach((line) => output.WriteLine(line));
-                        Console.WriteLine("Input file converted successfully!");
-                        return 0;
-                    }
 
-                }catch(Exception e)
-                {
-                    Console.WriteLine("Exception:" + e.Message);
-                }
-            }
-            return -1;
+    public static int Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("GWBasicConverter.exe input-file.bas output-file");
         }
+        else
+        {
+            using var input = new FileStream(args[0], FileMode.Open);
+            using var output = new StreamWriter(args[1]);
+            var code = new GWBasicCodeGenerator();
+            try
+            {
+                var lines = code.Parse(input,Encoding.UTF8); //Encoding.GetEncoding(437)
+                if (lines != null)
+                {
+                    lines.ForEach(output.WriteLine);
+                    Console.WriteLine("Input file converted successfully!");
+                    return 0;
+                }
+
+            }catch(Exception e)
+            {
+                Console.WriteLine("Exception:" + e.Message);
+            }
+        }
+        return -1;
     }
 }

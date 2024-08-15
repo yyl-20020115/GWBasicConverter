0ab9b0b7b3ed791ae76147f0381704c17704e1aa
Yilin Yang <yyl_20050115@hotmail.com>
2024-08-15 15:04:02 +0800

    added folder conversion function

--- GWBasicConverter/Program.cs
+++ GWBasicConverter/Program.cs
@@ -4,33 +4,82 @@ namespace GWBasicConverter;
 
 public class Program
 {
-
     public static int Main(string[] args)
     {
         if (args.Length < 2)
         {
-            Console.WriteLine("GWBasicConverter.exe input-file.bas output-file");
+            Console.WriteLine("GWBasicConverter.exe \"input-file.bas\" \"output-file\"");
+            Console.WriteLine("                  or \"input-folder\" \"output-folder\"");
         }
         else
         {
-            using var input = new FileStream(args[0], FileMode.Open);
-            using var output = new StreamWriter(args[1]);
-            var code = new GWBasicCodeGenerator();
-            try
+            var src = args[0];
+            var dst = args[1];
+            if (Directory.Exists(src))
             {
-                var lines = code.Parse(input,Encoding.UTF8); //Encoding.GetEncoding(437)
-                if (lines != null)
+                if (!Directory.Exists(dst))
                 {
-                    lines.ForEach(output.WriteLine);
-                    Console.WriteLine("Input file converted successfully!");
-                    return 0;
+                    try
+                    {
+                        Directory.CreateDirectory(dst);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("Exception:" + e.Message);
+                    }
+
                 }
 
-            }catch(Exception e)
+                foreach (var file in Directory.GetFiles(src, "*.BAS"))
+                {
+                    if (File.ReadAllBytes(file) is byte[] buffer && buffer.Length>0 
+                        && buffer[0]== GWBasicCodeGenerator.MagicByte)
+                    {
+                        var name = Path.GetFileNameWithoutExtension(file);
+                        Convert(file, Path.Combine(dst, $"{name}_Converted.BAS"));
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"Input file \"{file}\" is not an encoded file.");
+                    }
+                }
+                return 0;
+            }
+            else if (File.Exists(src))
             {
-                Console.WriteLine("Exception:" + e.Message);
+                if (File.ReadAllBytes(src) is byte[] buffer && buffer.Length > 0
+                        && buffer[0] == GWBasicCodeGenerator.MagicByte)
+                {
+                    return Convert(src, dst) ? 0 : -1;
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Input file \"{src}\" is not an encoded file.");
+                    return -1;
+                }
             }
         }
         return -1;
+    }
+    public static bool Convert(string src, string dst)
+    {
+        using var input = new FileStream(src, FileMode.Open);
+        using var output = new StreamWriter(dst);
+        var code = new GWBasicCodeGenerator();
+        try
+        {
+            var lines = code.Parse(input, Encoding.UTF8); //Encoding.GetEncoding(437)
+            if (lines != null)
+            {
+                lines.ForEach(output.WriteLine);
+                Console.WriteLine($"Input file \"{src}\" converted to \"{dst}\" successfully!");
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine("Exception:" + e.Message);
+        }
+        return false;
     }
 }
